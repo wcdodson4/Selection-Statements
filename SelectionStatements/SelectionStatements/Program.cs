76f68ff2d8a44f8c2821ba646b87b7a6728e8087
William Dodson <wcdodson4@gmail.com>
2020-01-21 12:47:02 -0600

    added stuff

---
+++ SelectionStatements/SelectionStatements/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number.");
+            var favNumber = 4;
+            var guess = int.Parse(Console.ReadLine());
+            if (guess < favNumber)
+                Console.WriteLine("Too low. Continue?");
+            else if (guess > favNumber)
+                Console.WriteLine("Too high. Continue?");
+            else
+                Console.WriteLine("Nevermind.");
+        }
+    }
+}
