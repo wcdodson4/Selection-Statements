af253cc7bf28edce88ab46a46f8059d880959db1
William Dodson <wcdodson4@gmail.com>
2020-01-22 10:24:10 -0600

    loops

--- SelectionStatements/SelectionStatements/Program.cs
+++ SelectionStatements/SelectionStatements/Program.cs
@@ -72,6 +72,13 @@ namespace SelectionStatements
             //modified code:
             var answer = 4;
             var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+        
+            //loops
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
+
     }
 }
