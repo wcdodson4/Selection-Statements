8d51fa7688470099eda744aa9d5b5f7579b072a8
William Dodson <wcdodson4@gmail.com>
2020-01-22 09:47:55 -0600

    added syntax sugar

--- SelectionStatements/SelectionStatements/Program.cs
+++ SelectionStatements/SelectionStatements/Program.cs
@@ -5,7 +5,7 @@ namespace SelectionStatements
     class Program
     {
         static void Main(string[] args)
-        {
+        {   ///do-while loop
             Console.WriteLine("Try and guess my favorite number.");
             var favNumber = 32;
             var guess = int.Parse(Console.ReadLine());
@@ -53,6 +53,25 @@ namespace SelectionStatements
                 }
             }
             while (guess != favNumber);
+
+            //syntax sugar exercise
+
+            //original code:
+            //int answer = 4;
+            //string response;
+
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + "is less than nine";
+            //}
+
+            //modified code:
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
         }
     }
 }
