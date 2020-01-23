ad4f28d9082358e071f0e1fb35952bc71df52eb6
William Dodson <wcdodson4@gmail.com>
2020-01-23 13:15:28 -0600

    dinosaurs

---
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraysAndLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a list named dinosaurs
+            //Add 3 different dinosaur names to the list
+            //print all 3 names from that list to the console - *using a loop*
+
+            var dinosaurs = new List<string>();
+            dinosaurs.Add("ankylosaurus");
+            dinosaurs.Add("spinosaurus");
+            dinosaurs.Add("stegosaurus");
+            
+            foreach (var dino in dinosaurs)
+            {
+                Console.WriteLine(dino);
+            }
+        }
+    }
+}
