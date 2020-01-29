1e53365326519567865e172b40bec49c7dec515f
William Dodson <wcdodson4@gmail.com>
2020-01-29 09:26:08 -0600

    encapsulation

---
+++ Encapsulation/Encapsulation/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation
+{
+    class BankAccount
+    {
+        private double Balance = 0;
+
+        public double Deposit(double depositAmount)
+        {
+            return Balance += depositAmount;
+        }
+
+        public double WithDraw(double withdrawAmount)
+        {
+            return Balance -= withdrawAmount;
+        }
+
+        public void GetBalance()
+        {
+            Console.WriteLine($"You have ${Balance} in your bank account.");
+        }
+    }
+}
---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var balance = new BankAccount();
+            balance.Deposit(100);
+            balance.WithDraw(58);
+            balance.GetBalance();
+        }
+    }
+}
